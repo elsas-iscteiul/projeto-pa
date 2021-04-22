f89f6af82542c9bbe7d5fdb4f4625715f031c4c2
elsas-iscteiul <elsas@iscte-iul.pt>
2021-04-23 00:26:22 +0100

    wip fase2

--- ProgramacaoAvancada/src/main/kotlin/Fase2.kt
+++ ProgramacaoAvancada/src/main/kotlin/Fase2.kt
@@ -4,6 +4,27 @@ import kotlin.reflect.full.declaredMemberProperties
 
 data class Aluno(val nome: String, val numero: Int)
 
+fun generateJson(name: String, col : Collection<*>): JsonObject{
+    val finalValue = JsonObject()
+    finalValue.addElement(JsonElement(name, col))
+    return finalValue
+}
+
+fun generateJson(m : Map<*,*>): JsonObject{
+    val jObject = JsonObject()
+    m.forEach{
+        when{
+            it.value is Int -> jObject.addElement(JsonElement(it.key.toString(), it.value as Int))
+            it.value is Boolean -> jObject.addElement(JsonElement(it.key.toString(), it.value as Boolean))
+            it.value is String -> jObject.addElement(JsonElement(it.key.toString(), it.value as String))
+            it.value is Collection<*> -> jObject.addElement(JsonElement(it.key.toString(), it.value as Collection<*>))
+        }
+    }
+    return jObject
+}
+
+
+
 fun generateJson(a: Any) : JsonObject{
     val c: KClass<Any> = a::class as KClass<Any>
     val key = c.simpleName
@@ -25,8 +46,10 @@ fun generateJson(a: Any) : JsonObject{
 
 
 fun main(){
-    val a = Aluno("Pedro", 99233)
-    val jObject = generateJson(a)
+    //val a = Aluno("Pedro", 99233)
+    //val jObject = generateJson(a)
+    val mapTest = mapOf("Joao" to listOf<Int>(1,2,3), "Manuela" to listOf(4,5,6))
+    val jObject = generateJson(mapTest)
     val textSerializer = VisitorTextSerialize()
     jObject.accept(textSerializer)
     println(textSerializer.serializedText)
